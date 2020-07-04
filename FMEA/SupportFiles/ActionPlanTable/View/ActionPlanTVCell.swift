c9da508ffbef039bff601fa245dfc6b778f1d927
Stanislav Berezin <stanislav.berezin@icloud.com>
2020-07-04 21:58:27 +0300

    1) Structure of the project is created 2) Controllers are added

---
+++ FMEA/SupportFiles/ActionPlanTable/View/ActionPlanTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActionPlanTVCell.swift
+//  FMEA
+//
+//  Created by BEREZIN Stanislav on 04.07.2020.
+//  Copyright © 2020 BEREZIN Stanislav. All rights reserved.
+//
+
+import UIKit
+
+class ActionPlanTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FMEA/SupportFiles/FmeaCollection/View/FmeaCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  FmeaCollectionCell.swift
+//  FMEA
+//
+//  Created by BEREZIN Stanislav on 04.07.2020.
+//  Copyright © 2020 BEREZIN Stanislav. All rights reserved.
+//
+
+import UIKit
+
+class FmeaCollectionCell: UICollectionViewCell {
+    
+}
